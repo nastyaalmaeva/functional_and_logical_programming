f966d6c438f4ba7d2342a29efd6e3101ec1f5d47
nastyaalmaeva <nastyaalmaeva@mail.ru>
2025-03-18 23:49:35 +0300

    Task 2 (Added function for solving quadratic equation) (LW1)

--- IntroductionToFSharp/Program.fs
+++ IntroductionToFSharp/Program.fs
@@ -1,7 +1,11 @@
 ﻿open System
 
+let solve (a, b, c) =
+    let D = b * b - 4.0 * a * c
+    (((-b + sqrt(D)) / (2.0 * a)), ((-b - sqrt(D)) / (2.0 * a)))
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World!"
-    Console.WriteLine("Hello World!")
+    Console.WriteLine(solve (1.0, 2.0, -3.0))
     0
