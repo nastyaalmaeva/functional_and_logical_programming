b9d6f13a8256a3580dd565511a8bf9b57ce5bd4d
nastyaalmaeva <nastyaalmaeva@mail.ru>
2025-03-19 00:15:03 +0300

    Task 2 (Solve fuction: all options are considered) (LW1)

--- IntroductionToFSharp/Program.fs
+++ IntroductionToFSharp/Program.fs
@@ -1,11 +1,25 @@
 ﻿open System
 
+type SolveResult =
+    None
+    | Linear of float
+    | Quadratic of float*float
+
 let solve a b c =
     let D = b * b - 4.0 * a * c
-    (((-b + sqrt(D)) / (2.0 * a)), ((-b - sqrt(D)) / (2.0 * a)))
+    if a = 0.0 then
+        if b = 0.0 then None
+        else Linear(-c / b)
+    else
+        if D < 0.0 then None
+        else Quadratic((((-b + sqrt(D)) / (2.0 * a)), ((-b - sqrt(D)) / (2.0 * a))))
 
 
 [<EntryPoint>]
 let main argv =
-    Console.WriteLine(solve 1.0 2.0 -3.0)
+    let result = solve 1.0 -2.0 1.0
+    match result with
+        None -> Console.WriteLine("There are no solutions.")
+        | Linear(x) -> Console.WriteLine($"Linear equation: {x}.")
+        | Quadratic(x1, x2) -> Console.WriteLine($"Quadratic equation: {x1}, {x2}.")
     0
