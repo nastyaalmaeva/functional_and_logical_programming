33bde48b697ede6ba63926d3b5ffea2159d64434
nastyaalmaeva <nastyaalmaeva@mail.ru>
2025-03-26 15:53:20 +0300

    Task 10 (numberTraversalWithCondition function: test under three conditions) (LW1)

--- HigherOrderFunctionsAndRecursions/Program.fs
+++ HigherOrderFunctionsAndRecursions/Program.fs
@@ -53,4 +53,8 @@ let main argv =
     Console.WriteLine($"Mul of digits of number: {numberTraversal 124 (fun x y -> (x * y)) 1}")
     Console.WriteLine($"Minimum digit of number: {numberTraversal 421 (fun x y -> if x > y then y else x) 10}")
     Console.WriteLine($"Maximum digit of number: {numberTraversal 421 (fun x y -> if x < y then y else x) -1}")
+    Console.WriteLine()
+    Console.WriteLine($"Sum of even digits of number: {numberTraversalWithCondition 4561 (fun x y -> (x + y)) 0 (fun digit -> digit % 2 = 0)}")
+    Console.WriteLine($"Mul of digits larger than 3: {numberTraversalWithCondition 4562 (fun x y -> (x * y)) 1 (fun digit -> digit > 3)}")
+    Console.WriteLine($"Minimum odd digit of number: {numberTraversalWithCondition 4562 (fun x y -> if x > y then y else x) 10 (fun digit -> digit % 2 <> 0)}")
     0   
